c5307419dff14546160b7e1d4c4d821a9519ea04
smayr <mayr97.sm@gmx.at>
2022-11-11 09:14:42 +0100

    changed Assignee Icon, removed unnecessary stuff from TaskDetail

--- src/components/custom-ui-elements/list-items/assignee-item.tsx
+++ src/components/custom-ui-elements/list-items/assignee-item.tsx
@@ -1,17 +1,37 @@
 import {TeamMemberData} from "../../../interfaces/team-member-data";
+import {UserCircleIcon} from "@heroicons/react/20/solid";
 
 type AssigneeItemProps = {
     person: TeamMemberData;
 };
 
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(" ");
+}
+
+function setColor(id: number){
+    switch (id%3) {
+        case 0:
+            return "text-green-500";
+            break;
+        case 1:
+            return "text-red-500";
+            break;
+        case 2:
+            return "text-blue-500";
+            break;
+    }
+}
+
 export const AssigneeItem = ({person}: AssigneeItemProps) => {
     return (
         <div className="flex items-center space-x-3">
             <div className="flex-shrink-0">
-                <img
-                    className="h-5 w-5 rounded-full"
-                    src="https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
-                    alt=""
+                <UserCircleIcon
+                    className={classNames(
+                        setColor(person.id) ?? "text-green-500",
+                        "h-5 w-5 rounded-full"
+                    )}
                 />
             </div>
             <div className="text-sm font-medium text-gray-900">
